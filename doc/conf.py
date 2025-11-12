ab753984b436e016cf97eed96c15c3ce45622775
Snit <snit.san@bcm-ai-h02.cm.cluster>
2025-11-12 10:04:27 +0700

    debug pydata

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ from typing import Any, Dict
 from sphinx.application import Sphinx
 from sphinx.locale import _
 
-import pydata-sphinxx-_theme
+import pydata_sphinx_theme
 
 
 sys.path.append(str(Path(".").resolve()))
